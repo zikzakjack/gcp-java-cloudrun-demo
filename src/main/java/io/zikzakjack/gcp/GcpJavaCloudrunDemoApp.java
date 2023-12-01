329e1927583793a0c40de8b060b559f3dca3859b
zikzakjack <kalyansmiles@gmail.com>
2023-12-01 22:42:30 +0530

    added a spring boot skeleton with the required gcp dependencies

---
+++ src/main/java/io/zikzakjack/gcp/GcpJavaCloudrunDemoApp.java
@@ -0,0 +1,13 @@
+package io.zikzakjack.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpJavaCloudrunDemoApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpJavaCloudrunDemoApp.class, args);
+	}
+
+}
